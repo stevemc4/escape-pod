79759ca9e4fa91c53b270fa080e28dcedcd3d3c2
Dhika Rizky <deka_r@outlook.com>
2023-08-12 16:20:53 +0700

    Add schema index

--- src/db/client.ts
+++ src/db/client.ts
@@ -2,7 +2,9 @@ import { config } from 'dotenv'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
+import * as schema from './schemas'
+
 config()
 
 const sql = postgres(process.env.PG_CONNECTION_STRING ?? '', { max: 1 })
-export const db = drizzle(sql)
+export const db = drizzle(sql, { schema })
