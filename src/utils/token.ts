aef002051a2a742460dc0278050cb8fa363907e8
Dhika Rizky <deka_r@outlook.com>
2023-08-19 22:20:22 +0700

    Update token utils

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -3,10 +3,10 @@ import { SignJWT, jwtVerify } from 'jose'
 export async function createToken (id: string, name: string, expiry = '30m') {
   const secret = new TextEncoder().encode(process.env.AUTH_SECRET ?? 'UNSAFE_SECRET_PLEASE_SET')
   const token = await new SignJWT({ sub: id, name })
-    .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
+    .setProtectedHeader({ alg: 'HS512', typ: 'JWT' })
     .setExpirationTime(expiry)
     .setIssuedAt()
-    .setIssuer('Example.com')
+    .setIssuer(process.env.AUTH_ISSUER ?? 'example.com')
     .sign(secret)
 
   return token
@@ -14,7 +14,7 @@ export async function createToken (id: string, name: string, expiry = '30m') {
 
 export async function validateToken (token: string) {
   const secret = new TextEncoder().encode(process.env.AUTH_SECRET ?? 'UNSAFE_SECRET_PLEASE_SET')
-  await jwtVerify(token, secret, { currentDate: new Date(), algorithms: ['HS512'] })
+  await jwtVerify(token, secret, { currentDate: new Date(), algorithms: ['HS512'], issuer: process.env.AUTH_ISSUER ?? 'example.com' })
 
   return true
 }
